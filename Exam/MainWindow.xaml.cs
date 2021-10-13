0dccd903dfbc28bddcb5d501bf4d7223d74fed17
Gulzada98 <akmsral2002@gmail.com>
2021-10-13 11:52:57 +0600

    initial commit

---
+++ Exam/MainWindow.xaml.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exam
+{
+    public partial class MainWindow : Window
+    {
+        NotifyIcon notifyIcon = new NotifyIcon();
+        public MainWindow()
+        {
+            InitializeComponent();
+            notifyIcon.Visible = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Visibility = Visibility.Hidden;
+
+            notifyIcon.BalloonTipText = "Waiting for notification...";
+            notifyIcon.ShowBalloonTip(1000);
+            if (WindowState == WindowState.Normal)
+            {
+                Hide();
+                notifyIcon.Visible = true;
+            }
+
+            notifyIcon.Icon = new Icon("C:\\Users\\Гульзада\\Desktop\\Step\\1.ico");
+
+            Connect();
+        }
+
+        public string Connect()
+        {
+            string responseData = String.Empty;
+            try
+            {
+                int port = 5001;
+                Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, port);
+
+                server.Bind(endPoint);
+
+                /*  TcpClient client = new TcpClient("127.0.0.1", port);
+
+                  NetworkStream stream = client.GetStream();*/
+
+                byte[] data = new byte[1024];
+
+                //int bytes = stream.Read(data, 0, data.Length);
+                EndPoint endPoint_2 = endPoint;
+                int bytes = server.ReceiveFrom(data, ref endPoint_2);
+                responseData = Encoding.UTF8.GetString(data, 0, bytes);
+
+                notifyIcon.BalloonTipText = responseData;
+                notifyIcon.ShowBalloonTip(1000);
+                string logMessage = "Successfully received notification! " + " Time: " + DateTime.Now + "\n";
+                CreateLog(logMessage);
+
+                server.Close();
+
+                /* stream.Close();
+                 client.Close();*/
+            }           
+            catch (SocketException e)
+            {
+                string logMessage = "SocketException: " + e + " Time: " + DateTime.Now + "\n";
+                CreateLog(logMessage);               
+            }
+                        
+            return responseData;
+        }
+
+        public void CreateLog(string logMessage)
+        {
+            try
+            {
+                string file_url = "/logs/log.txt";
+                
+                FileStream fs = new FileStream(file_url, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.ReadWrite);
+                                
+                byte[] data = Encoding.UTF8.GetBytes(logMessage);
+                fs.Write(data, 0, data.Length);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = null;
+            try
+            {                
+                int port = 5001;
+                IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+                                
+                server = new TcpListener(localAddr, port);
+                                
+                server.Start();
+                                
+                byte[] bytes = new byte[1024];               
+                                
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+                    NetworkStream stream = client.GetStream();
+                
+                    
+                    Console.WriteLine("Enter str");
+                    string msg = Console.ReadLine();                    
+                   
+                    byte[] data = Encoding.UTF8.GetBytes(msg);
+                                        
+                    stream.Write(data, 0, data.Length);
+                    Console.WriteLine("Sent: {0}", msg);
+                    Console.ReadKey();
+                                        
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+            finally
+            {                
+                server.Stop();
+            }
+
+            Console.WriteLine("\nHit enter to continue...");
+            Console.Read();
+        }
+    }
+}
